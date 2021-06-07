57d65105b7c345a55d7e14db7a78069fdea78e79
check5004 <check50045366@outlook.jp>
2021-06-08 08:51:40 +0900

    設定：デザイン追加

--- PLSQLcopy/Form1.Designer.cs
+++ PLSQLcopy/Form1.Designer.cs
@@ -24,6 +24,7 @@ namespace PLSQLcopy {
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.StartSqlPlus = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -31,13 +32,22 @@ namespace PLSQLcopy {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.設定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sQLPlusファイルパス指定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxSqlPlusPath = new System.Windows.Forms.ToolStripTextBox();
+            this.デフォルトに戻すToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // StartSqlPlus
             // 
-            this.StartSqlPlus.Location = new System.Drawing.Point(12, 152);
+            this.StartSqlPlus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StartSqlPlus.Location = new System.Drawing.Point(12, 166);
             this.StartSqlPlus.Name = "StartSqlPlus";
-            this.StartSqlPlus.Size = new System.Drawing.Size(211, 35);
+            this.StartSqlPlus.Size = new System.Drawing.Size(214, 35);
             this.StartSqlPlus.TabIndex = 1;
             this.StartSqlPlus.Text = "起動";
             this.StartSqlPlus.UseVisualStyleBackColor = true;
@@ -45,34 +55,46 @@ namespace PLSQLcopy {
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 32);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 46);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(211, 19);
+            this.textBox1.Size = new System.Drawing.Size(214, 19);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 78);
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(12, 92);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(211, 19);
+            this.textBox2.Size = new System.Drawing.Size(214, 19);
             this.textBox2.TabIndex = 3;
             this.textBox2.Text = "PRACTICE";
             this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 121);
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(12, 135);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(211, 19);
+            this.textBox3.Size = new System.Drawing.Size(214, 19);
             this.textBox3.TabIndex = 4;
             this.textBox3.Text = "LOG1441";
             this.textBox3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 17);
+            this.label1.Location = new System.Drawing.Point(10, 31);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(45, 12);
             this.label1.TabIndex = 5;
@@ -80,8 +102,11 @@ namespace PLSQLcopy {
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 63);
+            this.label2.Location = new System.Drawing.Point(10, 77);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 12);
             this.label2.TabIndex = 6;
@@ -89,18 +114,60 @@ namespace PLSQLcopy {
             // 
             // label3
             // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(10, 106);
+            this.label3.Location = new System.Drawing.Point(10, 120);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(43, 12);
             this.label3.TabIndex = 7;
             this.label3.Text = "テーブル";
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.設定ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(238, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 設定ToolStripMenuItem
+            // 
+            this.設定ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sQLPlusファイルパス指定ToolStripMenuItem});
+            this.設定ToolStripMenuItem.Name = "設定ToolStripMenuItem";
+            this.設定ToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.設定ToolStripMenuItem.Text = "設定(&I)";
+            // 
+            // sQLPlusファイルパス指定ToolStripMenuItem
+            // 
+            this.sQLPlusファイルパス指定ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.textBoxSqlPlusPath,
+            this.デフォルトに戻すToolStripMenuItem});
+            this.sQLPlusファイルパス指定ToolStripMenuItem.Name = "sQLPlusファイルパス指定ToolStripMenuItem";
+            this.sQLPlusファイルパス指定ToolStripMenuItem.Size = new System.Drawing.Size(200, 22);
+            this.sQLPlusファイルパス指定ToolStripMenuItem.Text = "SQL Plus ファイルパス指定";
+            // 
+            // textBoxSqlPlusPath
+            // 
+            this.textBoxSqlPlusPath.Font = new System.Drawing.Font("Yu Gothic UI", 9F);
+            this.textBoxSqlPlusPath.Name = "textBoxSqlPlusPath";
+            this.textBoxSqlPlusPath.Size = new System.Drawing.Size(180, 23);
+            // 
+            // デフォルトに戻すToolStripMenuItem
+            // 
+            this.デフォルトに戻すToolStripMenuItem.Name = "デフォルトに戻すToolStripMenuItem";
+            this.デフォルトに戻すToolStripMenuItem.Size = new System.Drawing.Size(240, 22);
+            this.デフォルトに戻すToolStripMenuItem.Text = "デフォルトに戻す";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(238, 197);
+            this.ClientSize = new System.Drawing.Size(238, 213);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -108,9 +175,14 @@ namespace PLSQLcopy {
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.StartSqlPlus);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SQL Plus＋";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,6 +196,11 @@ namespace PLSQLcopy {
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 設定ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sQLPlusファイルパス指定ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox textBoxSqlPlusPath;
+        private System.Windows.Forms.ToolStripMenuItem デフォルトに戻すToolStripMenuItem;
     }
 }
 
